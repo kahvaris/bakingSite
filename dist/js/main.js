068b6a59d2440ebd89e1a7131105a75f1d6c7145
variskay <kerttu.a.h.varis@student.jyu.fi>
2023-10-02 18:33:46 +0300

    Getting random recipe

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,55 @@
+
+const randomBtn = document.querySelector('.random-btn');
+
+randomBtn.addEventListener('click', getRandomRecipe);
+
+function getRandomRecipe() {
+    localStorage.setItem('runCode', 'true');
+    document.location.href = "recipes.html";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toRunOrNot = localStorage.getItem('runCode');
+
+    if (toRunOrNot === 'true') {
+        const recipes = document.querySelectorAll('.recipe');
+        const recipe = recipes[Math.floor(Math.random()*recipes.length)];
+    
+        const recipeHeader = recipe.querySelector('.recipe-header');
+        
+        const time = recipeHeader.querySelector('.time');
+        const recipeHead = recipeHeader.querySelector('.recipe-head');
+    
+        const recipeText = recipe.querySelector('.recipe-text');
+        const recipeImg = recipe.querySelector('.recipe-image');
+    
+        const rhContent = recipeHead.textContent;
+        const timeContent = time.textContent;
+        const tContent = recipeText.textContent;
+        const iContent = recipeImg.src;
+    
+        const postHead = document.querySelector('.post-header');
+        postHead.textContent = rhContent;
+    
+        const postDate = document.querySelector('.post-time');
+        postDate.textContent = timeContent;
+    
+        const postImg = document.querySelector('.post-image')
+        postImg.src = iContent;
+    
+        const postText = document.querySelector('.post-text');
+        postText.textContent = tContent;
+    
+        post.classList.add('show');
+        postBtn.classList.add('close');
+
+        localStorage.removeItem('runCode');
+    }
+
+});
+
+
+
 const post = document.querySelector('.post');
 const postBtn = document.querySelector('.post-btn');
 
