61af44b9e5518e74f5916f6b207c62f39910abaf
variskay <kerttu.a.h.varis@student.jyu.fi>
2023-10-08 18:41:45 +0300

    Responsiveness and other additions

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,29 +17,35 @@ document.addEventListener('DOMContentLoaded', function() {
     
         const recipeHeader = recipe.querySelector('.recipe-header');
         
-        const time = recipeHeader.querySelector('.time');
         const recipeHead = recipeHeader.querySelector('.recipe-head');
     
         const recipeText = recipe.querySelector('.recipe-text');
         const recipeImg = recipe.querySelector('.recipe-image');
+        const recipeLink = recipe.querySelector('.recipe-link');
     
         const rhContent = recipeHead.textContent;
-        const timeContent = time.textContent;
         const tContent = recipeText.textContent;
         const iContent = recipeImg.src;
+
+        const lContent = recipeLink.href;
+        const lContentText = recipeLink.textContent;
     
         const postHead = document.querySelector('.post-header');
         postHead.textContent = rhContent;
-    
-        const postDate = document.querySelector('.post-time');
-        postDate.textContent = timeContent;
     
         const postImg = document.querySelector('.post-image')
         postImg.src = iContent;
     
         const postText = document.querySelector('.post-text');
         postText.textContent = tContent;
-    
+
+        const postLink = document.querySelector('.post-link');
+        postLink.href = lContent;
+        postLink.textContent = lContentText;
+
+        if (recipe.classList.contains('cursed')) {
+            post.classList.add('cursed');
+        }
         post.classList.add('show');
         postBtn.classList.add('close');
 
@@ -62,22 +68,21 @@ buttons.forEach(button => {
 function showRecipe(event) {
     const recipeHeader = event.target.querySelector('.recipe-header');
     
-    const time = recipeHeader.querySelector('.time');
     const recipeHead = recipeHeader.querySelector('.recipe-head');
 
     const recipeText = event.target.querySelector('.recipe-text');
     const recipeImg = event.target.querySelector('.recipe-image');
+    const recipeLink = event.target.querySelector('.recipe-link');
 
     const rhContent = recipeHead.textContent;
-    const timeContent = time.textContent;
     const tContent = recipeText.textContent;
     const iContent = recipeImg.src;
+    
+    const lContent = recipeLink.href;
+    const lContentText = recipeLink.textContent;
 
     const postHead = document.querySelector('.post-header');
     postHead.textContent = rhContent;
-
-    const postDate = document.querySelector('.post-time');
-    postDate.textContent = timeContent;
 
     const postImg = document.querySelector('.post-image')
     postImg.src = iContent;
@@ -85,6 +90,13 @@ function showRecipe(event) {
     const postText = document.querySelector('.post-text');
     postText.textContent = tContent;
 
+    const postLink = document.querySelector('.post-link');
+    postLink.href = lContent;
+    postLink.textContent = lContentText;
+
+    if (event.target.classList.contains('cursed')) {
+        post.classList.add('cursed');
+    }
     post.classList.add('show');
     postBtn.classList.add('close');
 }
@@ -95,4 +107,5 @@ postBtn.addEventListener('click', hideRecipe);
 function hideRecipe() {
     postBtn.classList.remove('close');
     post.classList.remove('show');
+    post.classList.remove('cursed');
 }
